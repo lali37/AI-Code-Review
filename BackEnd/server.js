39b37f3a3a4551c1cde9c5fae9b2c690b76f576e
Lalitha <148626084+lali37@users.noreply.github.com>
2025-10-30 22:07:36 +0530

    Update server log URL for deployment

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,5 +3,5 @@ const app = require('./src/app');
 
 
 app.listen(3000, () => {
-    console.log('Server is running on https://localhost:3000')
+    console.log('Server is running on https://ai-code-review-backend-2pxg.onrender.com')
 })
