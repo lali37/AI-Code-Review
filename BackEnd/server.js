2ed8c4a2d6617840082032c1e12587f81b521904
Lalitha <148626084+lali37@users.noreply.github.com>
2025-10-31 10:55:55 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,16 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require('dotenv').config()
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3000;
 
-// ✅ Enable CORS so your frontend can access the backend
-app.use(cors({
-  origin: "https://ai-code-review-frontend-coy2.onrender.com", // your frontend link
-  methods: ["GET", "POST"],
-}));
-
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
-});
+app.listen(3000, () => {
+    console.log('Server is running on https://localhost:3000')
+})
